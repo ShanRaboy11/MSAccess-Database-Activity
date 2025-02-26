b88c95b6cd48f6d7da1bc4f32c4b1b9e979052c6
Shanii <shanraboy11@gmail.com>
2025-02-26 22:22:38 +0800

    refined each function

--- DatabaseActivity/Form1.Designer.cs
+++ DatabaseActivity/Form1.Designer.cs
@@ -63,48 +63,48 @@
             dgvStudentInfo.BackgroundColor = Color.FromArgb(248, 230, 189);
             dgvStudentInfo.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             dgvStudentInfo.ContextMenuStrip = contextMenuStrip1;
-            dgvStudentInfo.Location = new Point(23, 46);
+            dgvStudentInfo.Location = new Point(23, 58);
             dgvStudentInfo.Name = "dgvStudentInfo";
             dgvStudentInfo.ReadOnly = true;
             dgvStudentInfo.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
-            dgvStudentInfo.Size = new Size(671, 497);
+            dgvStudentInfo.Size = new Size(671, 439);
             dgvStudentInfo.TabIndex = 0;
             dgvStudentInfo.CellClick += dgvStudentInfo_CellClick;
             // 
             // contextMenuStrip1
             // 
-            contextMenuStrip1.Font = new Font("Segoe UI", 10F);
+            contextMenuStrip1.Font = new Font("Helvetica Neue", 10.749999F);
             contextMenuStrip1.Items.AddRange(new ToolStripItem[] { insertToolStripMenuItem, updateToolStripMenuItem, deleteToolStripMenuItem });
             contextMenuStrip1.Name = "contextMenuStrip1";
-            contextMenuStrip1.Size = new Size(124, 76);
+            contextMenuStrip1.Size = new Size(181, 92);
             // 
             // insertToolStripMenuItem
             // 
             insertToolStripMenuItem.Name = "insertToolStripMenuItem";
-            insertToolStripMenuItem.Size = new Size(123, 24);
+            insertToolStripMenuItem.Size = new Size(180, 22);
             insertToolStripMenuItem.Text = "Insert";
             insertToolStripMenuItem.Click += btnInsert_Click;
             // 
             // updateToolStripMenuItem
             // 
             updateToolStripMenuItem.Name = "updateToolStripMenuItem";
-            updateToolStripMenuItem.Size = new Size(123, 24);
+            updateToolStripMenuItem.Size = new Size(180, 22);
             updateToolStripMenuItem.Text = "Update";
             updateToolStripMenuItem.Click += btnUpdate_Click;
             // 
             // deleteToolStripMenuItem
             // 
             deleteToolStripMenuItem.Name = "deleteToolStripMenuItem";
-            deleteToolStripMenuItem.Size = new Size(123, 24);
+            deleteToolStripMenuItem.Size = new Size(180, 22);
             deleteToolStripMenuItem.Text = "Delete";
             deleteToolStripMenuItem.Click += btnDelete_Click;
             // 
             // btnConnect
             // 
-            btnConnect.Font = new Font("Segoe UI", 11F);
-            btnConnect.Location = new Point(14, 15);
+            btnConnect.Font = new Font("Helvetica Neue", 11.9999981F);
+            btnConnect.Location = new Point(13, 13);
             btnConnect.Name = "btnConnect";
-            btnConnect.Size = new Size(134, 26);
+            btnConnect.Size = new Size(144, 33);
             btnConnect.TabIndex = 1;
             btnConnect.Text = "Connection Test";
             btnConnect.UseVisualStyleBackColor = true;
@@ -112,10 +112,10 @@
             // 
             // btnLoad
             // 
-            btnLoad.Font = new Font("Segoe UI", 11F);
-            btnLoad.Location = new Point(873, 479);
+            btnLoad.Font = new Font("Helvetica Neue", 11.9999981F);
+            btnLoad.Location = new Point(169, 13);
             btnLoad.Name = "btnLoad";
-            btnLoad.Size = new Size(113, 26);
+            btnLoad.Size = new Size(120, 32);
             btnLoad.TabIndex = 2;
             btnLoad.Text = "Load File";
             btnLoad.UseVisualStyleBackColor = true;
@@ -123,10 +123,10 @@
             // 
             // btnUpdate
             // 
-            btnUpdate.Font = new Font("Segoe UI", 11F);
-            btnUpdate.Location = new Point(821, 213);
+            btnUpdate.Font = new Font("Helvetica Neue", 11.9999981F);
+            btnUpdate.Location = new Point(113, 120);
             btnUpdate.Name = "btnUpdate";
-            btnUpdate.Size = new Size(78, 26);
+            btnUpdate.Size = new Size(78, 32);
             btnUpdate.TabIndex = 3;
             btnUpdate.Text = "Update";
             btnUpdate.UseVisualStyleBackColor = true;
@@ -134,45 +134,45 @@
             // 
             // menuStrip1
             // 
-            menuStrip1.Font = new Font("Segoe UI", 11F);
+            menuStrip1.Font = new Font("Helvetica Neue", 11.9999981F, FontStyle.Regular, GraphicsUnit.Point, 204);
             menuStrip1.Items.AddRange(new ToolStripItem[] { connectionTestToolStripMenuItem, loadFileToolStripMenuItem });
             menuStrip1.Location = new Point(0, 0);
             menuStrip1.Name = "menuStrip1";
-            menuStrip1.Size = new Size(1024, 28);
+            menuStrip1.Size = new Size(1046, 26);
             menuStrip1.TabIndex = 4;
             menuStrip1.Text = "menuStrip1";
             // 
             // connectionTestToolStripMenuItem
             // 
             connectionTestToolStripMenuItem.Name = "connectionTestToolStripMenuItem";
-            connectionTestToolStripMenuItem.Size = new Size(130, 24);
+            connectionTestToolStripMenuItem.Size = new Size(136, 22);
             connectionTestToolStripMenuItem.Text = "Connection Test ";
             connectionTestToolStripMenuItem.Click += btnConnect_Click;
             // 
             // loadFileToolStripMenuItem
             // 
             loadFileToolStripMenuItem.Name = "loadFileToolStripMenuItem";
-            loadFileToolStripMenuItem.Size = new Size(81, 24);
+            loadFileToolStripMenuItem.Size = new Size(82, 22);
             loadFileToolStripMenuItem.Text = "Load File";
             loadFileToolStripMenuItem.Click += btnLoad_Click;
             // 
             // tbxID
             // 
             tbxID.BorderStyle = BorderStyle.None;
-            tbxID.Font = new Font("Segoe UI", 11F);
-            tbxID.Location = new Point(814, 91);
+            tbxID.Font = new Font("Helvetica Neue", 12.5F);
+            tbxID.Location = new Point(112, 15);
             tbxID.Name = "tbxID";
-            tbxID.Size = new Size(174, 20);
+            tbxID.Size = new Size(174, 19);
             tbxID.TabIndex = 5;
             // 
             // label1
             // 
             label1.AutoSize = true;
             label1.BackColor = Color.FromArgb(157, 214, 231);
-            label1.Font = new Font("Segoe UI", 11F);
-            label1.Location = new Point(13, 13);
+            label1.Font = new Font("Helvetica Neue", 12.25F);
+            label1.Location = new Point(13, 16);
             label1.Name = "label1";
-            label1.Size = new Size(82, 20);
+            label1.Size = new Size(93, 19);
             label1.TabIndex = 6;
             label1.Text = "Student ID:";
             // 
@@ -180,48 +180,48 @@
             // 
             label2.AutoSize = true;
             label2.BackColor = Color.FromArgb(157, 214, 231);
-            label2.Font = new Font("Segoe UI", 11F);
-            label2.Location = new Point(11, 52);
+            label2.Font = new Font("Helvetica Neue", 12.25F);
+            label2.Location = new Point(11, 51);
             label2.Name = "label2";
-            label2.Size = new Size(82, 20);
+            label2.Size = new Size(94, 19);
             label2.TabIndex = 7;
             label2.Text = "Last Name:";
             // 
             // tbxLname
             // 
             tbxLname.BorderStyle = BorderStyle.None;
-            tbxLname.Font = new Font("Segoe UI", 11F);
-            tbxLname.Location = new Point(814, 130);
+            tbxLname.Font = new Font("Helvetica Neue", 12.5F);
+            tbxLname.Location = new Point(112, 50);
             tbxLname.Name = "tbxLname";
-            tbxLname.Size = new Size(174, 20);
+            tbxLname.Size = new Size(174, 19);
             tbxLname.TabIndex = 8;
             // 
             // label3
             // 
             label3.AutoSize = true;
             label3.BackColor = Color.FromArgb(157, 214, 231);
-            label3.Font = new Font("Segoe UI", 11F);
-            label3.Location = new Point(13, 92);
+            label3.Font = new Font("Helvetica Neue", 12.25F);
+            label3.Location = new Point(13, 86);
             label3.Name = "label3";
-            label3.Size = new Size(83, 20);
+            label3.Size = new Size(93, 19);
             label3.TabIndex = 9;
             label3.Text = "First Name:";
             // 
             // tbxFname
             // 
             tbxFname.BorderStyle = BorderStyle.None;
-            tbxFname.Font = new Font("Segoe UI", 11F);
-            tbxFname.Location = new Point(814, 169);
+            tbxFname.Font = new Font("Helvetica Neue", 12.5F);
+            tbxFname.Location = new Point(113, 85);
             tbxFname.Name = "tbxFname";
-            tbxFname.Size = new Size(174, 20);
+            tbxFname.Size = new Size(174, 19);
             tbxFname.TabIndex = 10;
             // 
             // btnDelete
             // 
-            btnDelete.Font = new Font("Segoe UI", 11F);
-            btnDelete.Location = new Point(907, 213);
+            btnDelete.Font = new Font("Helvetica Neue", 11.9999981F);
+            btnDelete.Location = new Point(208, 120);
             btnDelete.Name = "btnDelete";
-            btnDelete.Size = new Size(78, 26);
+            btnDelete.Size = new Size(78, 32);
             btnDelete.TabIndex = 11;
             btnDelete.Text = "Delete";
             btnDelete.UseVisualStyleBackColor = true;
@@ -229,10 +229,10 @@
             // 
             // btnInsert
             // 
-            btnInsert.Font = new Font("Segoe UI", 11F);
-            btnInsert.Location = new Point(734, 213);
+            btnInsert.Font = new Font("Helvetica Neue", 11.9999981F);
+            btnInsert.Location = new Point(737, 180);
             btnInsert.Name = "btnInsert";
-            btnInsert.Size = new Size(78, 26);
+            btnInsert.Size = new Size(78, 32);
             btnInsert.TabIndex = 12;
             btnInsert.Text = "Insert";
             btnInsert.UseVisualStyleBackColor = true;
@@ -243,40 +243,40 @@
             panel1.BackColor = Color.FromArgb(157, 214, 231);
             panel1.Controls.Add(label3);
             panel1.Controls.Add(label1);
+            panel1.Controls.Add(btnUpdate);
+            panel1.Controls.Add(btnDelete);
             panel1.Controls.Add(label2);
-            panel1.Location = new Point(715, 77);
+            panel1.Controls.Add(tbxFname);
+            panel1.Controls.Add(tbxID);
+            panel1.Controls.Add(tbxLname);
+            panel1.Location = new Point(718, 60);
             panel1.Name = "panel1";
-            panel1.Size = new Size(287, 174);
+            panel1.Size = new Size(306, 163);
             panel1.TabIndex = 13;
             // 
             // panel2
             // 
             panel2.BackColor = Color.FromArgb(157, 214, 231);
             panel2.Controls.Add(btnConnect);
-            panel2.Location = new Point(715, 464);
+            panel2.Controls.Add(btnLoad);
+            panel2.Location = new Point(718, 438);
             panel2.Name = "panel2";
-            panel2.Size = new Size(287, 56);
+            panel2.Size = new Size(306, 59);
             panel2.TabIndex = 14;
             // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(7F, 17F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.FromArgb(190, 229, 176);
-            ClientSize = new Size(1024, 567);
+            ClientSize = new Size(1046, 522);
             ContextMenuStrip = contextMenuStrip1;
             Controls.Add(btnInsert);
-            Controls.Add(btnDelete);
-            Controls.Add(tbxFname);
-            Controls.Add(tbxLname);
-            Controls.Add(tbxID);
-            Controls.Add(btnUpdate);
-            Controls.Add(btnLoad);
             Controls.Add(dgvStudentInfo);
             Controls.Add(menuStrip1);
             Controls.Add(panel1);
             Controls.Add(panel2);
-            Font = new Font("Segoe UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            Font = new Font("Helvetica Neue", 9.749999F, FontStyle.Regular, GraphicsUnit.Point, 204);
             Icon = (Icon)resources.GetObject("$this.Icon");
             MainMenuStrip = menuStrip1;
             Name = "Form1";
--- DatabaseActivity/Form1.cs
+++ DatabaseActivity/Form1.cs
@@ -21,7 +21,7 @@ namespace DatabaseActivity
 
         private void btnConnect_Click(object sender, EventArgs e)
         {
-            myConn = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source= C:\\Users\\Shan Michael\\OneDrive\\??\\2nd Year 2nd Sem\\OOP2\\RABOY, SHAN MICHAEL V. [SchoolDatabase].accdb");
+            myConn = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source= C:\\Users\\Shan Michael\\source\\repos\\RABOY, SHAN MICHAEL V. [SchoolDatabase].accdb");
             ds = new DataSet();
             myConn.Open();
             System.Windows.Forms.MessageBox.Show("Connected successfully!");
@@ -30,14 +30,9 @@ namespace DatabaseActivity
 
         private void btnLoad_Click(object sender, EventArgs e)
         {
-            myConn = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source= C:\\Users\\Shan Michael\\OneDrive\\??\\2nd Year 2nd Sem\\OOP2\\RABOY, SHAN MICHAEL V. [SchoolDatabase].accdb");
+            myConn = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source= C:\\Users\\Shan Michael\\source\\repos\\RABOY, SHAN MICHAEL V. [SchoolDatabase].accdb");
 
             da = new OleDbDataAdapter("SELECT *FROM Student", myConn);
-            //or da = new OleDbDataAdapter("SELECT Student.LastName, Student.FirstName, 
-            //Student.Course, SubjectsEnrolled.*, FinalGrade.StudentID\r\nFROM (Student 
-            //INNER JOIN SubjectsEnrolled ON Student.StudentID = 
-            //SubjectsEnrolled.StudentID) INNER JOIN FinalGrade ON Student.StudentID = 
-            //FinalGrade.StudentID", myConn);
             ds = new DataSet();
             myConn.Open();
             da.Fill(ds, "Student");
@@ -53,7 +48,14 @@ namespace DatabaseActivity
             cmd.Parameters.AddWithValue("@FName", tbxFname.Text);
             myConn.Open();
             cmd.ExecuteNonQuery();
+            da = new OleDbDataAdapter("SELECT *FROM Student", myConn);
+            ds = new DataSet();
+            da.Fill(ds, "Student");
+            dgvStudentInfo.DataSource = ds.Tables["Student"];
             myConn.Close();
+            tbxID.Text = "";
+            tbxLname.Text = "";
+            tbxFname.Text = "";
         }
 
         private void dgvStudentInfo_CellClick(object sender, DataGridViewCellEventArgs e)
@@ -61,6 +63,8 @@ namespace DatabaseActivity
             indexRow = e.RowIndex;
             DataGridViewRow row = dgvStudentInfo.Rows[indexRow];
             tbxID.Text = row.Cells[0].Value.ToString();
+            tbxLname.Text = row.Cells[1].Value.ToString();
+            tbxFname.Text = row.Cells[2].Value.ToString();
         }
 
         private void btnDelete_Click(object sender, EventArgs e)
@@ -71,18 +75,33 @@ namespace DatabaseActivity
             dgvStudentInfo.CurrentRow.Cells[0].Value);
             myConn.Open();
             cmd.ExecuteNonQuery();
+            da = new OleDbDataAdapter("SELECT *FROM Student", myConn);
+            ds = new DataSet();
+            da.Fill(ds, "Student");
+            dgvStudentInfo.DataSource = ds.Tables["Student"];
             myConn.Close();
+            tbxID.Text = "";
+            tbxLname.Text = "";
+            tbxFname.Text = "";
         }
 
         private void btnUpdate_Click(object sender, EventArgs e)
         {
-            string query = "Update Student Set LastName = @Lname Where StudentID = @id";
+            string query = "Update Student Set LastName = @Lname, FirstName = @Fname Where StudentID = @id";
             cmd = new OleDbCommand(query, myConn);
-            cmd.Parameters.AddWithValue("@ln", tbxLname.Text);
+            cmd.Parameters.AddWithValue("@Lname", tbxLname.Text);
+            cmd.Parameters.AddWithValue("@Fname", tbxFname.Text); 
             cmd.Parameters.AddWithValue("@id", Convert.ToInt32(tbxID.Text));
             myConn.Open();
             cmd.ExecuteNonQuery();
+            da = new OleDbDataAdapter("SELECT *FROM Student", myConn);
+            ds = new DataSet();
+            da.Fill(ds, "Student");
+            dgvStudentInfo.DataSource = ds.Tables["Student"];
             myConn.Close();
+            tbxID.Text = "";
+            tbxLname.Text = "";
+            tbxFname.Text = "";
         }
     }
 }
